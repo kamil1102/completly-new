3385620f99a0998126af16c88f107c6f4922aa82
Kamil Tajchman <Kamil.Tajchman@apetito.co.uk>
2022-03-20 20:04:57 +0000

    Add project files.

---
+++ 555555/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _555555
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
