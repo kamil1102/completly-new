3dd806af7890f0ac8878b49fbb4e9f8d355397ae
Kamil Tajchman <Kamil.Tajchman@apetito.co.uk>
2022-03-20 20:06:42 +0000

    my firsyt chnge

--- 555555/Program.cs
+++ 555555/Program.cs
@@ -6,7 +6,7 @@ namespace _555555
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello !");
         }
     }
 }
